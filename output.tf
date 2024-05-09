5461b3a45c6c6419cc92ff175c0128ee7dfda655
Dhaval <dhajoshi@users.noreply.github.com>
2024-05-09 13:33:06 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "workspace_id" {
   description = "The ID of Terraform Cloud/Enterprise workspace"
-  value       = module.advanced_workspace.id
+  value       = module.advanced_workspace.workspace_id
 }
